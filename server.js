a38b1829d1ca2f414d5baaac9b1128a4d1e30db9
Giuliano <lucco74@gmail.com>
2022-11-17 20:23:17 -0300

    desafioC22

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const PORT = 8081;
-
-const path = require("path");
-const config = require("./config.js");
 const { Server: IOServer } = require("socket.io");
 const { Server: HttpServer } = require("http");
-<<<<<<< HEAD
 const ProductosC = require("./productosDb.js");
 const { Messages } = require("./DAOs/mensajesDaos.js");
-=======
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
-const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-const ProductosC = require("./productosDb");
-const { options } = require("./options/mariaDb");
-const { optionsSqlite } = require("./options/sqlite");
-const Messages = require("./menssagesDb");
->>>>>>> be862bdfdb486caf7c7523cbfa867f6ea9bb7c3a
 const Tables = require("./createTable.js");
 
 const app = express();
@@ -27,72 +13,16 @@ const io = new IOServer(httpServer);
 let prod = new ProductosC();
 const mens = new Messages();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    store: MongoStore.create({
-      mongoUrl: config.mongoRemote.cnxStr,
-      mongoOptions: advancedOptions,
-    }),
-    secret: "shhhhhhhhhhhhhhhhhhhh",
-    resave: false,
-    saveUninitialized: false,
-    rolling: true,
-    cookie: {
-      maxAge: 150000,
-    },
-  })
-);
-
-app.use(express.static("public"));
-
+app.use(express.static("./public"));
 app.get("/", (req, res) => {
-  res.render(path.join(process.cwd(), "/views/index.ejs"), {
-    nombre: req.session.nombre,
-  });
+  res.sendFile("index.html");
 });
 
-<<<<<<< HEAD
 app.get("/api/productos-test", async (req, res) => {
   const productosFaker = await prod.getAll();
   res.json(productosFaker);
 });
 
-=======
-app.get("/login", (req, res) => {
-  const nombre = req.session?.nombre;
-  if (nombre) {
-    res.redirect("/");
-  } else {
-    res.sendFile(path.join(process.cwd(), "./public/login.html"));
-  }
-});
-
-app.post("/login", (req, res) => {
-  const usuario = req.body?.nombre;
-  req.session.nombre = usuario;
-  res.redirect("/");
-});
-
-app.get("/logout", (req, res) => {
-  const nombre = req.session?.nombre;
-  if (nombre) {
-    req.session.destroy((err) => {
-      if (!err) {
-        res.render(path.join(process.cwd(), "/views/logout.ejs"), {
-          nombre,
-        });
-      } else {
-        res.redirect("/login");
-      }
-    });
-  } else {
-    res.redirect("/login");
-  }
-});
->>>>>>> be862bdfdb486caf7c7523cbfa867f6ea9bb7c3a
 io.on("connection", async (socket) => {
   console.log("Usuario conectado");
 
